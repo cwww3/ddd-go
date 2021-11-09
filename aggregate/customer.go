ede2af678c438f7ee70e94fa5143170a33785552
cwww3 <2239354893@qq.com>
2021-11-09 15:19:18 +0800

    ddd-go

---
+++ aggregate/customer.go
@@ -0,0 +1,73 @@
+// Package aggregates holds aggregates that combines many entities into a full object
+package aggregate
+
+import (
+	"errors"
+	"github.com/cwww3/ddd-go/entity"
+	"github.com/cwww3/ddd-go/valueobject"
+	"github.com/google/uuid"
+)
+
+var (
+	// ErrInvalidPerson is returned when the person is not valid in the NewCustome factory
+	ErrInvalidPerson = errors.New("a customer has to have an valid person")
+)
+
+// Customer is an aggregate that combines all entities needed to represent a customer
+type Customer struct {
+	// person is the root entity of a customer
+	// which means the person.ID is the main identifier for this aggregate
+	person *entity.Person
+	// a customer can hold many products
+	products []*entity.Item
+	// a customer can perform many transactions
+	transactions []valueobject.Transaction
+}
+
+// NewCustomer is a factory to create a new Customer aggregate
+// It will validate that the name is not empty
+func NewCustomer(name string) (Customer, error) {
+	// Validate that the Name is not empty
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	// Create a new person and generate ID
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+	// Create a customer object and initialize all the values to avoid nil pointer exceptions
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
+}
+
+//We need to add a way of retrieving information from the Customer aggregate, such as an ID from the root entity.
+//So we should update the aggregate with a little function for grabbing the ID, and a function to change the name.
+
+// GetID returns the customers root entity ID
+func (c *Customer) GetID() uuid.UUID {
+	return c.person.ID
+}
+// SetID sets the root ID
+func (c *Customer) SetID(id uuid.UUID) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+	c.person.ID = id
+}
+
+// SetName changes the name of the Customer
+func (c *Customer) SetName(name string) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+	c.person.Name = name
+}
+// SetName changes the name of the Customer
+func (c *Customer) GetName() string {
+	return c.person.Name
+}
